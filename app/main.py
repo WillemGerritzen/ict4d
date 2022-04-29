951fb52f5aec9bda3978250a1f272968e92ae80b
ZNBai <bzn991206@gmail.com>
2022-04-29 02:59:05 +0200

    test

--- app/main.py
+++ app/main.py
@@ -59,8 +59,7 @@ def add_to_db():
         #weather description
         description = data['weather'][0]['description']
         #report
-        weather_report = "The weather in " + CITY +" is currently " + description + 
-        ", and the temperature is " + temperature + " degrees Fahrenheit."
+        weather_report = "The weather in " + CITY +" is currently " + description + ", and the temperature is " + temperature + " degrees Fahrenheit."
 
         city_data = {"weather": "the weather in "}
     else:
