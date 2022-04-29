c592a01fd2448ace490a243e7772857a20df47d0
ZNBai <bzn991206@gmail.com>
2022-04-29 02:57:41 +0200

    test

--- app/main.py
+++ app/main.py
@@ -50,8 +50,20 @@ def add_to_db():
         #get json data
         data = response.json()
         main = data['main']
-        #wea
         #temperature
+        temperature = main['temp']
+        #humidity
+        humidity = main['humidity']
+        #air pressure
+        pressure = main['pressure']
+        #weather description
+        description = data['weather'][0]['description']
+        #report
+        weather_report = "The weather in " + CITY +" is currently " + description + 
+        ", and the temperature is " + temperature + " degrees Fahrenheit."
+
+        city_data = {"weather": "the weather in "}
+    else:
         
         city_data = {"weather": "Error in the HTTP request"}
 
